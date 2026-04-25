4b701cc1c4e729fd0bcb29c64f12d65d9b22d5e9
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2026-04-25 11:12:49 +0200

    refactor: clean up AppModule and improve auth structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './modules/auth/auth.controller';
 import { PrismaService } from './prisma/prisma.service';
-import { AuthService } from './modules/auth/auth.service';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AuthService],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
